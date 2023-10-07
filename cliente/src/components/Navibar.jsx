dc9be96e04c2e861f33ce17c38d543bcfc53ebbd
CristopherPaiz <cpaizl1@miumg.edu.gt>
2023-10-07 16:38:20 -0600

    Add shine bottom to NaviBar

--- cliente/src/components/Navibar.jsx
+++ cliente/src/components/Navibar.jsx
@@ -16,22 +16,95 @@ import { contexto } from "../context/ContextProvider.jsx";
 const Navibar = () => {
   const { changeTheme, theme } = useContext(contexto);
   return (
-    <Navbar isBordered maxWidth="full">
-      <NavbarContent>
-        <NavbarBrand>
-          <p>
-            <RouterLink
-              to={"/"}
-              className={`font-black text-left text-4xl  ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              CRP
-            </RouterLink>
-          </p>
-        </NavbarBrand>
-      </NavbarContent>
-      <NavbarContent className="hidden sm:flex gap-1 md:ml-6 " justify="end">
-        {theme === "dark" ? (
-          <Button onClick={() => changeTheme(theme)} variant="light" isIconOnly className="m-2">
+    <>
+      <Navbar maxWidth="full">
+        <NavbarContent>
+          <NavbarBrand>
+            <p>
+              <RouterLink
+                to={"/"}
+                className={`font-black text-left text-5xl  ${theme === "dark" ? "claro" : "oscuro"}`}
+              >
+                CRP
+              </RouterLink>
+            </p>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent className="hidden sm:flex gap-1 md:ml-6 " justify="end">
+          {theme === "dark" ? (
+            <Button onClick={() => changeTheme(theme)} variant="light" isIconOnly className="m-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                />
+              </svg>
+            </Button>
+          ) : (
+            <Button onClick={() => changeTheme(theme)} variant="light" isIconOnly className="m-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                />
+              </svg>
+            </Button>
+          )}
+          <NavbarItem isActive>
+            <Button className="bg-transparent hover:bg-neutral-200 dark:hover:bg-neutral-900">
+              <RouterLink
+                to={"/"}
+                className={`text-base  py-2 -mx-3 px-3 flex gap-1 font-bold ${
+                  theme === "dark" ? "claro" : "oscuro"
+                }`}
+              >
+                Inicio
+              </RouterLink>
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button className="bg-transparent hover:bg-neutral-200 dark:hover:bg-neutral-900" variant="flat">
+              <RouterLink
+                to={"/about"}
+                className={`text-base  py-2 -mx-3 px-3 flex gap-1 font-bold ${
+                  theme === "dark" ? "claro" : "oscuro"
+                }`}
+              >
+                ¿Quién soy?
+              </RouterLink>
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button className="bg-transparent hover:bg-neutral-200 dark:hover:bg-neutral-900">
+              <RouterLink
+                to={"/contact"}
+                className={`text-base py-2 -mx-3 px-3 flex gap-1 font-bold ${
+                  theme === "dark" ? "claro" : "oscuro"
+                }`}
+              >
+                Contacto
+              </RouterLink>
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <Dropdown placement="bottom-end" className="sm:hidden">
+          <DropdownTrigger className="sm:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -43,143 +116,79 @@ const Navibar = () => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
-          </Button>
-        ) : (
-          <Button onClick={() => changeTheme(theme)} variant="light" isIconOnly className="m-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-              />
-            </svg>
-          </Button>
-        )}
-        <NavbarItem isActive>
-          <Button className="bg-transparent hover:bg-neutral-100 dark:hover:bg-slate-100">
-            <RouterLink
-              to={"/"}
-              className={`py-2 -mx-3 px-3 flex gap-1 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              Inicio
-            </RouterLink>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button className="bg-transparent hover:bg-success-100" variant="flat">
-            <RouterLink
-              to={"/about"}
-              className={`py-2 -mx-3 px-3 flex gap-1 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              ¿Quién soy?
-            </RouterLink>
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button className="bg-transparent hover:bg-purple-100">
-            <RouterLink
-              to={"/contact"}
-              className={`py-2 -mx-3 px-3 flex gap-1 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              Contacto
-            </RouterLink>
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-      <Dropdown placement="bottom-end" className="sm:hidden">
-        <DropdownTrigger className="sm:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Menu Actions" variant="flat" className="font-bold">
-          <DropdownItem key="comunidad">
-            <Link
-              to={"/"}
-              className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              Inicio
-            </Link>
-          </DropdownItem>
-          <DropdownItem key="farmacia">
-            <Link
-              to={"/about"}
-              className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              ¿Quién soy?
-            </Link>
-          </DropdownItem>
-          <DropdownItem key="social">
-            <Link
-              to={"/contact"}
-              className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
-            >
-              Contacto
-            </Link>
-          </DropdownItem>
-          <DropdownItem key="dark" className="text-primary">
-            {theme === "dark" ? (
-              <Button onClick={() => changeTheme(theme)} variant="light">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                  />
-                </svg>
-                Modo Claro
-              </Button>
-            ) : (
-              <Button onClick={() => changeTheme(theme)} variant="light">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-                  />
-                </svg>
-                Modo Oscuro
-              </Button>
-            )}
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
-    </Navbar>
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Menu Actions" variant="flat" className="font-bold">
+            <DropdownItem key="comunidad">
+              <Link
+                to={"/"}
+                className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
+              >
+                Inicio
+              </Link>
+            </DropdownItem>
+            <DropdownItem key="farmacia">
+              <Link
+                to={"/about"}
+                className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
+              >
+                ¿Quién soy?
+              </Link>
+            </DropdownItem>
+            <DropdownItem key="social">
+              <Link
+                to={"/contact"}
+                className={`pr-28 pl-3 flex gap-2 font-bold ${theme === "dark" ? "claro" : "oscuro"}`}
+              >
+                Contacto
+              </Link>
+            </DropdownItem>
+            <DropdownItem key="dark" className="text-primary">
+              {theme === "dark" ? (
+                <Button onClick={() => changeTheme(theme)} variant="light">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                    />
+                  </svg>
+                  Modo Claro
+                </Button>
+              ) : (
+                <Button onClick={() => changeTheme(theme)} variant="light">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                    />
+                  </svg>
+                  Modo Oscuro
+                </Button>
+              )}
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Navbar>
+      <div className={`${theme === "dark" ? "lineClaro" : "lineOscuro"}`}></div>
+    </>
   );
 };
 
--- cliente/src/views/Homepage.jsx
+++ cliente/src/views/Homepage.jsx
@@ -1,34 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { contexto } from "../context/ContextProvider.jsx";
 
 const Homepage = () => {
+  const { theme } = useContext(contexto);
   return (
     <div>
-      <h1 className="font-black text-4xl oscuro">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
-        inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
-        harum non excepturi sapiente illum?
-      </h1>
-      <h1 className="font-black text-4xl oscuro">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
-        inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
-        harum non excepturi sapiente illum?
-      </h1>
-      <h1 className="font-black text-4xl oscuro">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
-        inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
-        harum non excepturi sapiente illum?
-      </h1>
-      <h1 className="font-black text-4xl oscuro">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
-        inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
-        harum non excepturi sapiente illum?
-      </h1>
-      <h1 className="font-black text-4xl oscuro">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
-        inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
-        harum non excepturi sapiente illum?
-      </h1>
-      <h1 className="font-black text-4xl oscuro">
+      <h1 className={`font-black text-4xl ${theme === "dark" ? "claro" : "oscuro"}`}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corrupti consequatur eos distinctio sunt
         inventore dolorem perferendis exercitationem, reprehenderit quaerat quae quibusdam? Quisquam id ipsum
         harum non excepturi sapiente illum?
